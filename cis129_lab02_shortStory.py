b0de707f812468ed2e53ca6ea4ec9eaea845b61a
jlohrpima <jlohr@mail.pima.edu>
2025-02-11 21:33:58 -0700

    Update cis129_lab02_shortStory.py
    This was an excellent story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# author: Josh Lohr
+# lab 2 in cis129
+# really late at night
+# prints a short story that's probably dumb
+print("There's a village, hidden near a pond, full of tiny frogs")
+print("There's one frog named blob, who has a top hat (that's very important)")
+print("Blob is tasked with harvesting mushrooms, so his village can have a feast")
+print("He fails")
